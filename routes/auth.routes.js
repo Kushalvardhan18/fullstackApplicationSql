3bdbfa4d12a12c1be7e8986dc641f7167fb4d0f4
Kushal Vardhan <vardhank140102@gmail.com>
2025-04-27 22:04:14 +0530

    more userRoutes structure

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,11 +1,15 @@
-import { logInUser, registerUser, verifyUser } from "../controllers/auth.controller.js";
+import { forgotPassword, getProfile, logInUser, logOut, registerUser, resetPassword, verifyUser } from "../controllers/auth.controller.js";
 
 import express from "express"
 
 const userRouter = express.Router()
 
-userRouter.post("/register",registerUser)
-userRouter.post("/login",logInUser)
-userRouter.get("/verify/:token",verifyUser)
+userRouter.post("/register", registerUser)
+userRouter.post("/login", logInUser)
+userRouter.get("/verify/:token", verifyUser)
+userRouter.get("/profile", getProfile)
+userRouter.get("/logout", logOut)
+userRouter.get("/forgotpassword", forgotPassword)
+userRouter.get("/resetpassword", resetPassword)
 
 export default userRouter
